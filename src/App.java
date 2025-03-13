8b5e21460cfd82540beb0db556f88af9523133e0
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-13 20:18:42 -0300

    finalização do projeto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, DevOps Toledo Prudente!");
+
+        System.out.println("Executando operações matemáticas");
+        System.out.println(Calculadora.dividir(10, 5));
+
+        System.out.println("Criando objeto do tipo Pessoa");
+        Pessoa pessoa = new Pessoa("John Doe", "johndoe@gmail.com", 25, "DEV", "(18)12345-6789");
+        pessoa.apresentarse();
     }
 }
