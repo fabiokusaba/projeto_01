a316adfd722bc02cc45cd481caef10b0c2983526
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-13 20:09:17 -0300

    atualização do print a ser exibido no console

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, DevOps Toledo Prudente!");
-        System.out.println("Estamos na branch de refatoração do código main.");
     }
 }
